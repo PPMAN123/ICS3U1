94c6ddf2e248e951a2895269cbd841e2794b253e
Ethan Zhou <ethanzhou76@gmail.com>
2023-03-04 15:44:43 -0500

    Completed Unit 2 Assignment

--- Unit 2/Assignment/ZhouEthanA1Code.cpp
+++ Unit 2/Assignment/ZhouEthanA1Code.cpp
@@ -1,3 +1,11 @@
+/*
+  Family Budget Assignment for Unit 2
+  Made by: Ethan Zhou
+  Made for: Ms.Wun
+  Class: ICS3U1
+  Purpose: Gathers information about a family's income and expenses and outputs a budget summary
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <stdlib.h>
@@ -30,6 +38,7 @@ int main(){
   cout << "Amount of House Insurance sold by " << parentTwoName << ": $ ";
   cin >> parentTwoHouseIncome;
   cout << "Amount of Car Insurance sold by " << parentTwoName << ": $ ";
+
   cin >> parentTwoCarIncome;
   float childWage, childHoursPerWeek, childWeeksPerYear, childTotalIncome;
   cout << "Hourly Rate of " << childName << ": $ ";
@@ -119,6 +128,7 @@ int main(){
 
   float remainingMoney = parentOneSalary + parentTwoTotalIncome + childTotalIncome - totalTax - moneyInvested - totalElectricityCost - totalPhoneBill - totalGasBill - totalMortgage;
 
+  //Remaining money calculations
   float groceriesMoney = remainingMoney * 0.6;
   float entertainmentMoney = remainingMoney * 0.25;
   float cashSavings = remainingMoney * 0.15;
@@ -131,5 +141,146 @@ int main(){
   int numberWidthInteger = parentOneSalary;
   string numberWidthString = to_string(numberWidthInteger);
   cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentOneSalary << endl;
+
+  prompt = "Income of " + parentTwoName + " :";
+  numberWidthInteger = parentTwoTotalIncome;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentTwoTotalIncome << endl;
+
+  prompt = "Income of " + childName + " :";
+  numberWidthInteger = childTotalIncome;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << childTotalIncome << endl;
   
+  cout << endl;
+
+
+  prompt = "Total Family Income:";
+  numberWidthInteger = parentOneSalary + parentTwoTotalIncome + childTotalIncome;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentOneSalary + parentTwoTotalIncome + childTotalIncome << endl;
+
+  prompt = "Average Family Income:";
+  numberWidthInteger = (parentOneSalary + parentTwoTotalIncome + childTotalIncome) / 3;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << (parentOneSalary + parentTwoTotalIncome + childTotalIncome) / 3 << endl;
+
+  cout << "\n-------------------------TAX------------------------------------------\n" << endl;
+
+  prompt = "Income of " + parentOneName + " :";
+  numberWidthInteger = parentOneTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentOneTax << endl;
+
+  prompt = "Income of " + parentTwoName + " :";
+  numberWidthInteger = parentTwoTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentTwoTax << endl;
+
+  prompt = "Income of " + childName + " :";
+  numberWidthInteger = childTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << childTax << endl;
+
+  cout << endl;
+
+  prompt = "Total Amount of Tax Paid:";
+  numberWidthInteger = totalTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalTax << endl;
+
+  prompt = "Overall Tax Rate:";
+  numberWidthInteger = totalTax * 100/(parentOneSalary + parentTwoTotalIncome + childTotalIncome);
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() + 1) << right << fixed << setprecision(1)  << totalTax * 100/(parentOneSalary + parentTwoTotalIncome + childTotalIncome) << '%' <<  endl;
+
+  prompt = "Total Net Income:";
+  numberWidthInteger = parentOneSalary + parentTwoTotalIncome + childTotalIncome - totalTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentOneSalary + parentTwoTotalIncome + childTotalIncome - totalTax << endl;
+
+  prompt = "Average Monthly Net Income:";
+  numberWidthInteger = (parentOneSalary + parentTwoTotalIncome + childTotalIncome - totalTax)/12;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << (parentOneSalary + parentTwoTotalIncome + childTotalIncome - totalTax)/12 << endl;
+
+  cout << "\n-------------------------INVESTMENT-----------------------------------\n" << endl;
+
+  prompt = "Price per Share:";
+  numberWidthInteger = a + (b / c);
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << a + (b / c) << endl;
+
+  prompt = "Amount of Investment:";
+  numberWidthInteger = moneyInvested;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << moneyInvested << endl;
+
+  prompt = "Amount of Investment:";
+  numberWidthString = to_string(sharesBought);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() + 4) << right << fixed << sharesBought << endl;
+
+  cout << "\n-------------------------EXPENSES-------------------------------------\n" << endl;
+
+  prompt = "Total Cost of Electricity Bill for the Year:";
+  numberWidthInteger = totalElectricityCost;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalElectricityCost << endl;
+
+  prompt = "Total Cost of Phone Bill for the Year:";
+  numberWidthInteger = totalPhoneBill;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalPhoneBill << endl;
+
+  prompt = "Total Cost of Gas Bill for the Year:";
+  numberWidthInteger = totalGasBill;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalGasBill << endl;
+
+  prompt = "Total Cost of Mortgage Payments for the Year:";
+  numberWidthInteger = totalMortgage;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalMortgage << endl;
+
+  cout << endl;
+
+  prompt = "Total Amount for Groceries:";
+  numberWidthInteger = groceriesMoney;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << groceriesMoney << endl;
+
+  prompt = "Total Amount for Entertainment:";
+  numberWidthInteger = entertainmentMoney;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << entertainmentMoney << endl;
+
+  prompt = "Total Amount of Cash Savings:";
+  numberWidthInteger = cashSavings;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << cashSavings << endl;
+
+  cout << "\n-------------------------SUMMARY--------------------------------------\n" << endl;
+
+  prompt = "Total Household Gross Income:";
+  numberWidthInteger = parentOneSalary + parentTwoTotalIncome + childTotalIncome;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << parentOneSalary + parentTwoTotalIncome + childTotalIncome << endl;
+
+  prompt = "Total Income Tax Paid:";
+  numberWidthInteger = totalTax;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << totalTax << endl;
+
+  prompt = "Total Expenses:";
+  numberWidthInteger = totalElectricityCost + totalPhoneBill + totalGasBill + totalMortgage;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 4) << right << fixed << '$' << setprecision(2)  << totalElectricityCost + totalPhoneBill + totalGasBill + totalMortgage + groceriesMoney + entertainmentMoney << endl;
+
+  prompt = "Total Reserve Funds:";
+  numberWidthInteger = moneyInvested + cashSavings;
+  numberWidthString = to_string(numberWidthInteger);
+  cout << prompt << setw(70-prompt.length() - numberWidthString.length() - 3) << right << fixed << '$' << setprecision(2)  << moneyInvested + cashSavings << endl;
+
+  cout << "\nThank you for using this program!\n" << endl;
+  return 0;
 }
